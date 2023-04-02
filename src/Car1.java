1867638fe17d78ca037f52e805c490ae18f68673
visnjaRe <visnja.repac@gmail.com>
2023-04-02 16:29:32 -0500

    this is first draft, let me know what to change

---
+++ src/Car1.java
@@ -0,0 +1,7 @@
+public class Car1 {
+
+    String model;
+    String make;
+    String year;
+    String price;
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+
+    public static void main(String[] args) {
+
+        Car1 model1= new Car1();
+
+        model1.model="bmw";
+        model1.make="x5";
+        model1.year="2023";
+        model1.price="$50000";
+
+        System.out.println("newest x5 on the market");
+
+    }
+}
